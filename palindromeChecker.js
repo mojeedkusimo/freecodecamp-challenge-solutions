2b00d9208c7e0455f4a4cfcbadff07a45dae0018
MOJEED KUSIMO <mkusimo90@gmail.com>
2020-08-22 13:35:35 +0100

    Updated palindrome checker

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -1,6 +1,18 @@
-palindrome = (str) => {
+let palindrome = (str) => {
+    let newString = str.replace(/[^A-Z0-9]/gi, '').toLowerCase();
+    let revString = '';
 
+    for (let i = newString.length - 1; i > -1; i--) {
+        revString += newString[i]
+    }
+
+    if (newString === revString) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 
-console.log(palindrome(''));
+console.log(palindrome('_eye'));
