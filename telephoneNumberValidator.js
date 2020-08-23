eca32b59dcc24b1e3369a16d326ecff77e266a84
MOJEED KUSIMO <mkusimo90@gmail.com>
2020-08-23 09:59:49 +0100

    completed telephone number validator challenge

---
+++ telephoneNumberValidator.js
@@ -0,0 +1,20 @@
+
+
+let telephoneCheck = (str) => {
+
+    const FIRSTPATTERN = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+    const SECONDPATTERN = /^1\s[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+    const THIRDPATTERN = /^1\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{4}$/;
+    const FOURTHPATTERN = /^[0-9]{10}$/;
+    const FIFTHPATTERN = /^1{0,1}\([0-9]{3}\)[0-9]{3}-[0-9]{4}$/;
+    const SIXTHPATTERN = /^1\s[0-9]{3}\s[0-9]{3}\s[0-9]{4}$/;
+
+    if (FIRSTPATTERN.test(str) || SECONDPATTERN.test(str) || THIRDPATTERN.test(str) || FOURTHPATTERN.test(str) || FIFTHPATTERN.test(str) || SIXTHPATTERN.test(str)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(telephoneCheck('1 333 333 3333'));
