c02f2c20241c55c9e6a79596515b4fea2dfaba79
mojeedkusimo <mkusimo90@gmail.com>
2020-11-04 20:32:15 +0100

    Responsive Web Design Project 5 98.5% done

--- responsive-web-design-projects/personal-portfolio-webpage/script.js
+++ responsive-web-design-projects/personal-portfolio-webpage/script.js
@@ -1,7 +1,43 @@
-let paraDeco = document.querySelectorAll("span.para-deco");
+// Option 1
+let projectImg = document.querySelectorAll(".project-img");
+let projectPara = document.querySelectorAll(".project-p");
+let projectSpan = document.querySelectorAll(".project-span");
+let socialMediaLinks = document.querySelectorAll("li.social-media-links");
+let moreProjects = document.querySelector("#more");
+let showAll = document.querySelector("#show-all a");
 
-for (let elem of paraDeco) {
-    elem.addEventListener("click", function() {
-        elem.style.display = "inline"; 
+
+for (let i = 0; i < projectImg.length; i++) {
+    projectImg[i].addEventListener("mouseover", function() {
+        projectSpan[i].style.visibility = "visible";
+        projectSpan[i].style.visibility = "visible";
+    });
+
+    projectImg[i].addEventListener("mouseout", function() {
+        projectSpan[i].style.visibility = "hidden";
+        projectSpan[i].style.visibility = "hidden";
     });
 }
+
+for (let i = 0; i < projectImg.length; i++) {
+    projectPara[i].addEventListener("mouseover", function() {
+        projectSpan[i].style.visibility = "visible";
+        projectSpan[i].style.visibility = "visible";
+    });
+
+    projectPara[i].addEventListener("mouseout", function() {
+        projectSpan[i].style.visibility = "hidden";
+        projectSpan[i].style.visibility = "hidden";
+    });
+}
+
+showAll.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (showAll.textContent === "Show more") {
+        moreProjects.style.display = "flex";
+        showAll.textContent = "Hide Projects";
+    } else {
+        moreProjects.style.display = "none";
+        showAll.textContent = "Show more";
+    }
+});
